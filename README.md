80cec2f28c716a87e409042b63365a5f3dcb5815
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2023-02-12 21:19:09 -0300

    fix error

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+pip install -r requirements.txt
+uvicorn server:app --reload
+```
--- akinator.py
+++ akinator.py
@@ -37,8 +37,9 @@ class Akinator:
         if not self._current_question.is_leaf():
             return
 
-        old_person = Node(self._current_question.value)
-        new_person = Node(name)
+        nex_depth = self._current_question.depth + 1
+        old_person = Node(self._current_question.value, depth=nex_depth)
+        new_person = Node(name, depth=nex_depth)
 
         self._current_question.value = f"É {feature.lower()}?"
         self._current_question.left = old_person
